a3b406dcea26e13d0c21726ebc765d5a9b81edad
yuuichi.teranishi <yuuichi.teranishi@3b9f0baf-4ed6-6926-9859-f39c27c9bd96>
2011-07-30 10:12:10 +0000

    Use received message for forwarding

--- trunk/sim/src/org/piax/ov/ovs/rrsg/RRSkipGraph.java
+++ trunk/sim/src/org/piax/ov/ovs/rrsg/RRSkipGraph.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -56,6 +57,15 @@ public class RRSkipGraph extends SkipGraph {
 	
 	protected Map<Object,Object> rangeSearchOp(Node startNode, Range searchRange, int level, boolean found) {
 	    return map((Object)SkipGraph.Arg.OP, (Object)Op.RANGE_SEARCH).map(SkipGraph.Arg.NODE, startNode).map(Arg.RANGE, searchRange).map(SkipGraph.Arg.LEVEL, level).map(Arg.FOUND, found);
+	}
+	
+	protected Map<Object,Object> rangeSearchOp(Map<Object,Object> mes, Range searchRange, int level, boolean found) {
+	    Map<Object,Object> newMes = new HashMap<Object,Object>();
+	    newMes.putAll(mes);
+	    newMes.put(Arg.RANGE, searchRange);
+	    newMes.put(SkipGraph.Arg.LEVEL, level);
+	    newMes.put(Arg.FOUND, found);
+	    return newMes;
 	}
 	
 	protected void onReceiveRangeSearchResult(Node sender, Map<Object,Object> arg) {
@@ -120,22 +130,22 @@ public class RRSkipGraph extends SkipGraph {
 	    }
 	}
 	
-	public void onReceiveRangeSearchOp(Node sender, Map<Object,Object> args) {
-	    Node startNode = (Node) args.get(SkipGraph.Arg.NODE);
-        Range searchRange = (Range) args.get(Arg.RANGE);
-        int level = (int)((Integer)args.get(SkipGraph.Arg.LEVEL));
-        boolean found = (boolean)((Boolean)args.get(Arg.FOUND));
-        List<Id> via = getVia(args);
+	public void onReceiveRangeSearchOp(Node sender, Map<Object,Object> mes) {
+	    Node startNode = (Node) mes.get(SkipGraph.Arg.NODE);
+        Range searchRange = (Range) mes.get(Arg.RANGE);
+        int level = (int)((Integer)mes.get(SkipGraph.Arg.LEVEL));
+        boolean found = (boolean)((Boolean)mes.get(Arg.FOUND));
+
         try {
             if (searchRange.includes(key)) {
-                onRangeMatch(sender, args);
+                onRangeMatch(sender, mes);
                 found = true;
             }
             if (compare(searchRange.min, key) < 0 && compare(key, searchRange.max) < 0) {
                 // self is included. divide into two ranges.
                 List<Range> divided = searchRange.divideC(key);
-                self.send(setVia(rangeSearchOp(startNode, divided.get(0), level, found), via));
-                self.send(setVia(rangeSearchOp(startNode, divided.get(1), level, found), via));
+                self.send(rangeSearchOp(mes, divided.get(0), level, found));
+                self.send(rangeSearchOp(mes, divided.get(1), level, found));
             }
             if (compare(key, searchRange.min) <= 0) {
                 // self is less than the range.
@@ -143,14 +153,14 @@ public class RRSkipGraph extends SkipGraph {
                     Comparable<?> rightKey = neighbors.getKey(R, level);
                     if (rightKey != null && compare(rightKey, searchRange.min) <= 0) {
                         System.out.println(String.format("R: KEY:%4s, LEVEL:%2d, MV:%s", key, level, m.toString()));
-                        neighbors.get(R, level).send(setVia(rangeSearchOp(startNode, searchRange, level, found), via));
+                        neighbors.get(R, level).send(rangeSearchOp(mes, searchRange, level, found));
                         break;
                     }
                     else {
                         if (rightKey != null && searchRange.includes(rightKey)) {
                             List<Range> divided = searchRange.divideR(rightKey);
                             System.out.println(String.format("R: KEY:%4s, LEVEL:%2d, MV:%s, Range: %s", key, level, m.toString(), divided.get(1)));
-                            neighbors.get(R, level).send(setVia(rangeSearchOp(startNode, divided.get(1), level, found), via));
+                            neighbors.get(R, level).send(rangeSearchOp(mes, divided.get(1), level, found));
                             searchRange = divided.get(0);
                         }
                         level--;
@@ -162,13 +172,13 @@ public class RRSkipGraph extends SkipGraph {
                 while (level >= 0) {
                     Comparable<?> leftKey = neighbors.getKey(L, level);
                     if (leftKey != null && compare(searchRange.max, leftKey) <= 0) {
-                        neighbors.get(L, level).send(setVia(rangeSearchOp(startNode, searchRange, level, found), via));
+                        neighbors.get(L, level).send(rangeSearchOp(mes, searchRange, level, found));
                         break;
                     }
                     else {
                         if (leftKey != null && searchRange.includes(leftKey)) {
                             List<Range> divided = searchRange.divideL(leftKey);
-                            neighbors.get(L, level).send(setVia(rangeSearchOp(startNode, divided.get(0), level, found), via));
+                            neighbors.get(L, level).send(rangeSearchOp(mes, divided.get(0), level, found));
                             searchRange = divided.get(1); 
                         }
                         level--;
@@ -176,12 +186,18 @@ public class RRSkipGraph extends SkipGraph {
                 }
             }
             if (level < 0 && !found) {
-                startNode.send(setVia(notFoundInRangeOp(self), via));
+                startNode.send(setVia(notFoundInRangeOp(self), getVia(mes)));
             }
         }
         catch (IOException e) {
             e.printStackTrace();
         }
     }
+	
+	private void dumpMes(Map<Object,Object> mes) {
+	    for (Object key: mes.keySet()) {
+	        System.out.println(key + ": " + mes.get(key));
+	    }
+	}
 	
 }
